68c2e15193b5de50ca65bb74a222333e17e6407d
Your Name <you@example.com>
2015-12-28 20:48:09 +0600

    init

---
+++ trunk/Projects/HongFei/mw/ffmpeg.h
@@ -0,0 +1,15 @@
+#ifndef __FFMPEG_H__
+#define __FFMPEG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libavdevice/avdevice.h"
+#ifdef __cplusplus
+}
+#endif
+
+#endif
